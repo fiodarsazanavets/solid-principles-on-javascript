302a6463edc40dddb5f6192327c3b828f448dc55
mobiletechtracker <info@mobiletechtracker.co.uk>
2022-05-16 16:05:16 +0100

    Add a folder for Liskov substitution principle samples.

---
+++ 3-liskov-substitution-principle/text-to-html-convertor/index.js
@@ -0,0 +1,32 @@
+const FileProcessor = require('./fileProcessor.js');
+const MdTextProcessor = require('./mdTextProcessor.js');
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const tagsToReplace = {
+    '**': {
+        opening: '<strong>',
+        closing: '</strong>' 
+    },
+    '*': {
+        opening: '<em>',
+        closing: '</em>' 
+    },
+    '~~': {
+        opening: '<del>',
+        closing: '</del>' 
+    }
+};
+
+readline.question('Please specify the file to convert to HTML.', path => {
+    var fileProcessor = new FileProcessor(path);
+    var texPtrocessor = new MdTextProcessor(tagsToReplace);
+
+    var inputText = fileProcessor.readAllText();
+    var outputText = texPtrocessor.convertText(inputText);
+    fileProcessor.writeToFile(outputText);
+    readline.close();
+});
---
+++ 3-liskov-substitution-principle/text-to-html-convertor/textProcessor.js
@@ -0,0 +1,18 @@
+class TextProcessor {
+
+    convertText(inputText) {
+        
+        var paragraphs = inputText.split(/[\r\n\?|\n]/);
+        var textToWrite = '';
+    
+        for (var i = 0; i < paragraphs.length; i++) {
+            if (paragraphs[i])
+                textToWrite += `<p>${paragraphs[i]}</p>\n`;
+        }
+    
+        textToWrite += '<br/>';
+        return textToWrite;
+    }
+}
+
+module.exports = TextProcessor;
